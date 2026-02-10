ae5476ddec73012bcfb208a36fef6fd96f3bd5b9
Roman Likhadievski <git@romanlee17.com>
2026-02-10 12:21:18 +0300

    crazygames xsolla support

---
+++ Runtime/Web/CrazyGamesXSollaPayments.cs
@@ -0,0 +1,141 @@
+using AOT;
+using MirraGames.SDK.Common;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Xsolla.Core;
+
+namespace MirraGames.SDK.XSolla.Web
+{
+    [Provider(typeof(IPayments))]
+    public class CrazyGamesXSollaPayments : CommonXSollaPayments
+    {
+        public CrazyGamesXSollaPayments(IData data) : base(data)
+        {
+            GetXSollaToken(
+                (token) => {
+                    Logger.CreateText(this, $"Successfully got XSolla token {token}");
+                    XsollaToken.Create(token);
+                    GetItems();
+                },
+                () => {
+                    Logger.CreateError(this, "Failed to get XSolla token from CrazyGames");
+                    GetItems();
+                }
+            );
+        }
+
+        [DllImport(Naming.InternalDll)]
+        private static extern bool crazyGames_isLoggedIn();
+
+        protected override bool IsLoggedIn()
+        {
+            return crazyGames_isLoggedIn();
+        }
+
+        [DllImport(Naming.InternalDll)]
+        private static extern void crazyGames_invokeLogin(int senderId, DelegateVoid onSuccess, DelegateVoid onError);
+
+        private record OnLoginSuccessInfo
+        {
+            public Action onSuccess;
+        }
+
+        private record OnLoginErrorInfo
+        {
+            public Action onError;
+        }
+
+        private static readonly Dictionary<int, OnLoginSuccessInfo> onLoginSuccessCallbacks = new();
+        private static readonly Dictionary<int, OnLoginErrorInfo> onLoginErrorCallbacks = new();
+        private static int nextLoginSenderId = 0;
+
+        [MonoPInvokeCallback(typeof(DelegateVoid))]
+        private static void OnLoginSuccess(int senderId)
+        {
+            if (onLoginSuccessCallbacks.TryGetValue(senderId, out OnLoginSuccessInfo onSuccessInfo))
+            {
+                onSuccessInfo.onSuccess?.Invoke();
+                onLoginSuccessCallbacks.Remove(senderId);
+            }
+            else
+            {
+                throw new ArgumentException($"SenderId {senderId} not found in collection");
+            }
+        }
+
+        [MonoPInvokeCallback(typeof(DelegateVoid))]
+        private static void OnLoginError(int senderId)
+        {
+            if (onLoginErrorCallbacks.TryGetValue(senderId, out OnLoginErrorInfo onErrorInfo))
+            {
+                onErrorInfo.onError?.Invoke();
+                onLoginErrorCallbacks.Remove(senderId);
+            }
+            else
+            {
+                throw new ArgumentException($"SenderId {senderId} not found in collection");
+            }
+        }
+
+        protected override void InvokeLogin(Action onSuccess, Action onError)
+        {
+            int senderId = nextLoginSenderId++;
+            onLoginSuccessCallbacks[senderId] = new OnLoginSuccessInfo { onSuccess = onSuccess };
+            onLoginErrorCallbacks[senderId] = new OnLoginErrorInfo { onError = onError };
+            crazyGames_invokeLogin(senderId, OnLoginSuccess, OnLoginError);
+        }
+
+        [DllImport(Naming.InternalDll)]
+        private static extern void crazyGames_getXSollaToken(int senderId, DelegateString onSuccess, DelegateVoid onError);
+
+        private record OnSuccessInfo
+        {
+            public Action<string> onSuccess;
+        }
+        private record OnErrorInfo
+        {
+            public Action onError;
+        }
+
+        private static readonly Dictionary<int, OnSuccessInfo> onTokenSuccessCallbacks = new();
+        private static readonly Dictionary<int, OnErrorInfo> onTokenErrorCallbacks = new();
+        private static int nextTokenSenderId = 0;
+
+        [MonoPInvokeCallback(typeof(DelegateString))]
+        private static void OnTokenSuccess(int senderId, string token)
+        {
+            if (onTokenSuccessCallbacks.TryGetValue(senderId, out OnSuccessInfo onSuccessInfo))
+            {
+                onSuccessInfo.onSuccess?.Invoke(token);
+                onTokenSuccessCallbacks.Remove(senderId);
+            }
+            else
+            {
+                throw new ArgumentException($"SenderId {senderId} not found in collection");
+            }
+        }
+
+        [MonoPInvokeCallback(typeof(DelegateVoid))]
+        private static void OnTokenError(int senderId)
+        {
+            if (onTokenErrorCallbacks.TryGetValue(senderId, out OnErrorInfo onErrorInfo))
+            {
+                onErrorInfo.onError?.Invoke();
+                onTokenErrorCallbacks.Remove(senderId);
+            }
+            else
+            {
+                throw new ArgumentException($"SenderId {senderId} not found in collection");
+            }
+        }
+
+        private static void GetXSollaToken(Action<string> onSuccess, Action onError)
+        {
+            int senderId = nextTokenSenderId++;
+            onTokenSuccessCallbacks[senderId] = new OnSuccessInfo { onSuccess = onSuccess };
+            onTokenErrorCallbacks[senderId] = new OnErrorInfo { onError = onError };
+            crazyGames_getXSollaToken(senderId, OnTokenSuccess, OnTokenError);
+        }
+    }
+}
---
+++ Runtime/Web/Plugins/crazyGames_xsolla_library.jslib
@@ -0,0 +1,72 @@
+const crazyGames_xsolla_library = {
+
+    crazyGames_getXSollaToken: function (senderId, onSuccessPtr, onErrorPtr) {
+        console.log('library', 'crazyGames_getXSollaToken');
+        const onSuccess = (tokenUTF8) => {
+            Module.invokeMonoPCallback(senderId, onSuccessPtr, tokenUTF8);
+        };
+        const onError = () => {
+            Module.invokeMonoPCallback(senderId, onErrorPtr);
+        };
+        try {
+            async function getXSollaToken() {
+                await Module.waitForMirraSDK();
+                try {
+                    const token = await window.CrazyGames.SDK.user.getXsollaUserToken();
+                    console.log('library', 'Get Xsolla token result', token);
+                    const tokenUTF8 = Module.allocateString(token);
+                    onSuccess(tokenUTF8);
+                } catch (error) {
+                    console.error('library', 'Error getting Xsolla token', error);
+                    onError();
+                }
+            }
+            getXSollaToken();
+        }
+        catch (error) {
+            console.error('library', error);
+            onError();
+        }
+    },
+
+    crazyGames_isLoggedIn: function () {
+        try {
+            if (Module.mirraSDK) {
+                return Module.mirraSDK.player.isLoggedIn;
+            }
+            else {
+                console.log('library', 'mirraSDK is not ready');
+                return false;
+            }
+        }
+        catch (error) {
+            console.error('crazyGames_xsolla_library', error);
+            return false;
+        }
+    },
+
+    crazyGames_invokeLogin: function (senderId, onSuccessPtr, onErrorPtr) {
+        console.log('library', 'crazyGames_invokeLogin');
+        const onSuccess = () => {
+            Module.invokeMonoPCallback(senderId, onSuccessPtr);
+        };
+        const onError = () => {
+            Module.invokeMonoPCallback(senderId, onErrorPtr);
+        };
+        try {
+            if (Module.mirraSDK) {
+                Module.mirraSDK.player.invokeLogin(onSuccess, onError);
+            }
+            else {
+                console.log('library', 'mirraSDK is not ready');
+                onError();
+            }
+        }
+        catch (error) {
+            console.error('library', error);
+            onError();
+        }
+    },
+
+};
+mergeInto(LibraryManager.library, crazyGames_xsolla_library);
